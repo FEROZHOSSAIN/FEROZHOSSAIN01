f58cba26ce157f3e77cc60d7a7cbd1981a1422d8
FEROZHOSSAIN <33205846+FEROZHOSSAIN@users.noreply.github.com>
2017-11-05 13:59:15 -0500

    Update Ferozhossain01.java

--- FEROZHOSSAIN01/src/FEROZHOSSAIN01/Ferozhossain01.java
+++ FEROZHOSSAIN01/src/FEROZHOSSAIN01/Ferozhossain01.java
@@ -5,6 +5,7 @@ public class Ferozhossain01 {
 	public static void main(String[] args) {
 		System.out.println("My first Git project");
 		System.out.println("jhjhjhjk");
+System.out.println("new updated");
 
 	}
 
